8989b331beb60645fdd81541d339c1a556090434
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-11-21 20:50:28 -0300

    Formulario de edição

--- Controllers/GestaoController.cs
+++ Controllers/GestaoController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Supermarket_system_with_ASP.NET_Core.Data;
+using Supermarket_system_with_ASP.NET_Core.DTO;
 
 namespace Supermarket_system_with_ASP.NET_Core.Controllers
 {
@@ -24,6 +25,14 @@ namespace Supermarket_system_with_ASP.NET_Core.Controllers
 
         public IActionResult NovaCategoria(){
             return View();
+        }
+
+        public IActionResult EditarCategoria(int Id){ //Recebendo o ID cateogria por parametro
+            var categoria = this._database.Categorias.First(categoria => categoria.Id == Id); // Buscando a informação da categoria no Banco de dados
+            CategoriaDTO categoriaView = new CategoriaDTO(); //Convertendo o objeto Categoria em um objeto CategoriaDTO (Que é o tipo de objeto que o formulario (view) lida)
+            categoriaView.Id = categoria.Id;
+            categoriaView.Nome = categoria.Nome;
+            return View(categoriaView);//Passando para a view o objeto resultando do banco de dados.
         }
 
         public IActionResult Fornecedores(){
