12e0324223a5424b219c79964e927fed21ff6b73
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-11-26 00:17:51 -0300

    Exibindo dados

--- wwwroot/js/frente.js
+++ wwwroot/js/frente.js
@@ -4,11 +4,23 @@
 var enderecoProduto = "https://localhost:5001/Produtos/Produto/";
 var produto;
 /* */
+
+/* Funções */
+function preencherFormulario(dadosProduto) {
+    $("#campoNome").val(dadosProduto.nome);
+    $("#campoCategoria").val(dadosProduto.categoria.nome)
+    $("#campoFornecedor").val(dadosProduto.fornecedor.nome)
+    $("#campoPreco").val(dadosProduto.precoDeVenda);
+    
+}
+/* */
 $("#pesquisar").click(function() {
     var codProduto = $("#codProduto").val();
     var enderecoTemporario = enderecoProduto + codProduto;
     $.post(enderecoTemporario, function(dados, status) {
         produto = dados;
+        console.log(produto)
+        preencherFormulario(produto);
     }).fail(function(){
         alert("Produto inválido");
     })
