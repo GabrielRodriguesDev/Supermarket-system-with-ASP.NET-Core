27cfb35be83a3180a326c6d2835835fb32e0fe06
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-11-28 19:37:48 -0300

    Carregando os produtos na tabela

--- wwwroot/js/frente.js
+++ wwwroot/js/frente.js
@@ -13,15 +13,73 @@ function preencherFormulario(dadosProduto) {
     $("#campoPreco").val(dadosProduto.precoDeVenda);
     
 }
-/* */
+
+function zerarFormulario () {
+    $("#codProduto").val("")
+    $("#campoNome").val("");
+    $("#campoCategoria").val("")
+    $("#campoFornecedor").val("")
+    $("#campoPreco").val("");
+    $("#campoQuantidade").val("");
+}
+
+
+function adicionarNaTabela(p,q) {
+    $("#compras").append(`
+    <tr>
+    <td>${p.id}</td>
+    <td>${p.nome}</td>
+    <td>${q}</td>
+    <td>${p.precoDeVenda}</td>
+    <td>${p.medicao}</td>
+    <td>${p.precoDeVenda * q}</td>
+    <td><button class="btn btn-danger"> Remover </button></td>
+</tr>
+    `);
+}
+
+$("#produtoForm").on("submit", function(event){
+    event.preventDefault();
+    console.log("Antes");
+    console.log(produto);
+    var produtoParaTabela = produto;
+
+    var qtd = $("#campoQuantidade").val();
+    if(qtd <= 0) {
+        alert("Informe a quantidade.")
+    } else {
+        //produto = undefined;
+        adicionarNaTabela(produtoParaTabela,qtd);
+        zerarFormulario();
+    }
+});
+
+
+/*Ajax */
 $("#pesquisar").click(function() {
     var codProduto = $("#codProduto").val();
     var enderecoTemporario = enderecoProduto + codProduto;
     $.post(enderecoTemporario, function(dados, status) {
         produto = dados;
-        console.log(produto)
+        var med = "";
+        switch (produto.medicao) {
+            case 0:
+                med = "LT";
+                break;
+            case 1:
+                med = "KG";
+                break;
+            case 2:
+                med = "UND";
+                break;
+            default:
+                med = "UND";
+                break;
+        }
+        produto.medicao = med;
         preencherFormulario(produto);
     }).fail(function(){
         alert("Produto inválido");
+        $("#codProduto").val("")
     })
 })
