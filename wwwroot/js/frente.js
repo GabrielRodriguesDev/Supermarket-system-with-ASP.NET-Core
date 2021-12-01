4747d0d30ef4f85849e50201f80ee2b0ca362abb
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-11-30 23:27:24 -0300

    Calculando o troco

--- wwwroot/js/frente.js
+++ wwwroot/js/frente.js
@@ -123,8 +123,9 @@ $("#finalizarVendaBtn").click( function(){
     if(!isNaN(_valorPago)) { // isNan() -> Válida se existe um número, por mais que seja do tipo string ele valida se existe um número / Se não for um numero retorna verdadeiro se for um numero retorna falso.
         _valorPago = parseFloat(_valorPago);
         if(_valorPago >= __totalVenda__) {
-            console.log(_valorPago);
-            console.log("Salve")
+            var _troco  = _valorPago - __totalVenda__;
+            console.log(_valorPago)
+            $("#valorTroco").val(_troco.toFixed(2));
         } else {
             alert("Valor pago inferior ao total da venda.")
             return;
